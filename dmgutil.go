b0b0a8439c522b40dc737a138b4e31cf444d963e
Stephan Baker <stephan.baker@gmail.com>
2016-02-24 16:18:44 -0500

    Moving CopyFile and CopyDirectory to github.com/forestgiant/fsutils package.

--- dmgutil.go
+++ dmgutil.go
@@ -5,123 +5,11 @@ package dmgutil
 
 import (
 	"errors"
-	"io"
-	"os"
 	"os/exec"
 	"strings"
+
+	"github.com/forestgiant/fsutil"
 )
-
-//CopyFile copies the file at the source path to the provided destination.
-func CopyFile(source, destination string) error {
-	//Validate the source and destination paths
-	if len(source) == 0 {
-		return errors.New("You must provide a source file path.")
-	}
-
-	if len(destination) == 0 {
-		return errors.New("You must provide a destination file path.")
-	}
-
-	//Verify the source path refers to a regular file
-	sourceFileInfo, err := os.Lstat(source)
-	if err != nil {
-		return err
-	}
-
-	//Handle regular files differently than symbolic links and other non-regular files.
-	if sourceFileInfo.Mode().IsRegular() {
-		//open the source file
-		sourceFile, err := os.Open(source)
-		if err != nil {
-			return err
-		}
-		defer sourceFile.Close()
-
-		//create the destinatin file
-		destinationFile, err := os.Create(destination)
-		if err != nil {
-			return err
-		}
-		defer destinationFile.Close()
-
-		//copy the source file contents to the destination file
-		if _, err = io.Copy(destinationFile, sourceFile); err != nil {
-			return err
-		}
-
-		//replicate the source file mode for the destination file
-		if err := os.Chmod(destination, sourceFileInfo.Mode()); err != nil {
-			return err
-		}
-	} else if sourceFileInfo.Mode()&os.ModeSymlink != 0 {
-		linkDestinaton, err := os.Readlink(source)
-		if err != nil {
-			return errors.New("Unable to read symlink. " + err.Error())
-		}
-
-		if err := os.Symlink(linkDestinaton, destination); err != nil {
-			return errors.New("Unable to replicate symlink. " + err.Error())
-		}
-	} else {
-		return errors.New("Unable to use io.Copy on file with mode " + string(sourceFileInfo.Mode()))
-	}
-
-	return nil
-}
-
-//CopyDirectory copies the directory at the source path to the provided destination, with the option of recursively copying subdirectories.
-func CopyDirectory(source string, destination string, recursive bool) error {
-	if len(source) == 0 || len(destination) == 0 {
-		return errors.New("File paths must not be empty.")
-	}
-
-	//get properties of the source directory
-	sourceInfo, err := os.Stat(source)
-	if err != nil {
-		return err
-	}
-
-	//create the destination directory
-	err = os.MkdirAll(destination, sourceInfo.Mode())
-	if err != nil {
-		return err
-	}
-
-	sourceDirectory, err := os.Open(source)
-	if err != nil {
-		return err
-	}
-	defer sourceDirectory.Close()
-
-	objects, err := sourceDirectory.Readdir(-1)
-	if err != nil {
-		return err
-	}
-
-	for _, object := range objects {
-		if object.Name() == ".Trashes" {
-			continue
-		}
-
-		sourceObjectName := source + "/" + object.Name()
-		destObjectName := destination + "/" + object.Name()
-
-		if object.IsDir() {
-			//create sub-directories
-			err = CopyDirectory(sourceObjectName, destObjectName, true)
-			if err != nil {
-				return err
-			}
-		} else {
-			err = CopyFile(sourceObjectName, destObjectName)
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
-}
 
 //Mount uses the `os/exec` package to issue an `hdiutil attach -nobrowse` command for the given OSX disk image (.dmg).
 func Mount(sourcePath string) (volumePath string, err error) {
@@ -161,7 +49,7 @@ func ExtractDMG(sourcePath, destinationPath string) error {
 		return errors.New("Failed to mount the volume. " + err.Error())
 	}
 
-	if err := CopyDirectory(volumePath, destinationPath, true); err != nil {
+	if err := fsutil.CopyDirectory(volumePath, destinationPath, true); err != nil {
 		return errors.New("Failed to copy contents of the volume. " + err.Error())
 	}
 
--- dmgutil_test.go
+++ dmgutil_test.go
@@ -5,86 +5,14 @@ import (
 	"testing"
 )
 
-func TestCopyFile(t *testing.T) {
-	var badSourceFile = "./TestFiles/FGFailedSourceFileLocation.fg"
-	var badDestinationFile = "./TestFiles/NonexistantDirectory/FGFailedDestFileLocation.fg"
-	var goodSourceFile = "./TestFiles/DiskImages/dmgutil test_0.0.1.dmg"
-	var goodDestinationFile = "./TestFiles/DiskImages/dmgutil test_0.0.1_copy.dmg"
-	var symlinkSourceFile = "./TestFiles/DiskImages/TestSubdirectory/TextFiles"
-	var symlinkDestinationFile = "./TestFiles/DiskImages/TestSubdirectory/TextFilesCopy.dmg"
-
-	var tests = []struct {
-		source       string
-		destination  string
-		shouldAssert bool
-	}{
-		{"", goodDestinationFile, false},
-		{goodSourceFile, "", false},
-		{badSourceFile, goodDestinationFile, false},
-		{goodSourceFile, badDestinationFile, false},
-		{goodSourceFile, goodDestinationFile, true},
-		{symlinkSourceFile, symlinkDestinationFile, true},
-	}
-
-	for index, test := range tests {
-		err := CopyFile(test.source, test.destination)
-		if err != nil && test.shouldAssert == true {
-			t.Errorf("Test %d failed but should have passed. "+err.Error(), index)
-		} else if err == nil && test.shouldAssert == false {
-			t.Errorf("Test %d passed but should have failed.", index)
-		} else if err == nil && test.shouldAssert == true {
-			if _, err := os.Stat(test.source); err != nil {
-				t.Error("CopyFile did not throw an error, but the test file was not created.")
-			}
-
-			if err := os.Remove(test.destination); err != nil {
-				t.Error("Unable to clean up after running CopyFile tests. " + err.Error())
-			}
-		}
-	}
-}
-
-func TestCopyDestinationFile(t *testing.T) {
-	var badSourceDirectory = "./TestFiles/BadSourceFolder"
-	var goodSourceDirectory = "./TestFiles/DiskImages"
-	var goodDestinationDirectory = "./TestFiles/DiskImagesCopy"
-
-	var tests = []struct {
-		source       string
-		destination  string
-		shouldAssert bool
-	}{
-		{"", goodDestinationDirectory, false},
-		{goodSourceDirectory, "", false},
-		{badSourceDirectory, goodDestinationDirectory, false},
-		{goodSourceDirectory, goodDestinationDirectory, true},
-	}
-
-	for index, test := range tests {
-		err := CopyDirectory(test.source, test.destination, true)
-		if err != nil && test.shouldAssert == true {
-			t.Errorf("Test %d failed but should have passed.", index)
-		} else if err == nil && test.shouldAssert == false {
-			t.Errorf("Test %d passed but should have failed.", index)
-		}
-	}
-
-	if _, err := os.Stat(goodDestinationDirectory); err != nil {
-		t.Error("CopyDirectory tests passed, but the test directory was not created.")
-	}
-
-	if err := os.RemoveAll(goodDestinationDirectory); err != nil {
-		t.Error("Unable to clean up after running CopyDirectory tests. " + err.Error())
-	}
-}
-
 func TestMount(t *testing.T) {
 	var tests = []struct {
 		path         string
 		shouldAssert bool
 	}{
-		{"./TestFiles/TextFiles/dmgutil test_0.0.1.txt", false},
-		{"./TestFiles/DiskImages/dmgutil test_0.0.1.dmg", true},
+		{"./TestFiles/test_0.0.1.txt", false},
+		{"./TestFiles/dmgutil test_0.0.1.txt", false},
+		{"./TestFiles/dmgutil test_0.0.1.dmg", true},
 	}
 
 	for index, test := range tests {
@@ -106,7 +34,7 @@ func TestMount(t *testing.T) {
 }
 
 func TestUnmount(t *testing.T) {
-	volumePath, err := Mount("./TestFiles/DiskImages/dmgutil test_0.0.1.dmg")
+	volumePath, err := Mount("./TestFiles/dmgutil test_0.0.1.dmg")
 	if err != nil {
 		t.Error("Unable to run TestUnmount.  Mount of test image failed.")
 		return
@@ -130,7 +58,7 @@ func TestUnmount(t *testing.T) {
 
 func TestExtractDMG(t *testing.T) {
 	var destinationDirectory = "./TestFiles/Applications"
-	if err := ExtractDMG("./TestFiles/DiskImages/dmgutil test_0.0.1.dmg", destinationDirectory); err != nil {
+	if err := ExtractDMG("./TestFiles/dmgutil test_0.0.1.dmg", destinationDirectory); err != nil {
 		t.Error("TestExtractDMG failed. " + err.Error())
 		return
 	}
